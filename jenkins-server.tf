76e49134f2be3a5d09ea00b69175dc8ee1c5431d
JendareyTechnologies <ak@jendareyconsulting.com>
2023-12-21 10:29:46 -0600

    Update jenkins-server.tf

--- jenkins-server.tf
+++ jenkins-server.tf
@@ -1,21 +1,17 @@
-# Resource: EC2 Instance
-resource "aws_instance" "jendarey-instance" {
-  ami                    = data.aws_ami.ubuntu_ami.id
-  instance_type          = var.INSTANCE[2]
-  key_name               = "automationkey"
-  user_data              = file("${path.module}/jenkins-server-script.sh")
-  availability_zone      = var.ZONE1
-  subnet_id              = aws_subnet.jendarey_public_subnet_a.id
-  vpc_security_group_ids = [aws_security_group.jendarey-security_group.id]
-  depends_on             = [aws_vpc.jendarey_vpc]
-  for_each               = toset(var.COUNT)
+# Resource: EC2 Instance for Jenkins
+resource "aws_instance" "jenkins-server-instance" {
+  ami                         = data.aws_ami.ubuntu_ami.id
+  instance_type               = var.INSTANCE[1]
+  key_name                    = "automationkey"
+  user_data                   = file("${path.module}/jenkins-server-script.sh")
+  monitoring                  = true
+  associate_public_ip_address = true  
+  subnet_id                   = aws_subnet.jendarey_public_subnet_a.id  
+  vpc_security_group_ids      = [aws_default_security_group.jendarey-default-sg.id]
 
   tags = {
-    Name    = each.value
-    Project = "Jendarey"
-  }
-
-  root_block_device {
-    volume_size = 10
+    Name        = "jenkins-server-instance"
+    Terraform   = "true"
+    Environment = "prod"
   }
 }
