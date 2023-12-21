795b15c0b5c71e55a649717aa4f1f0874100ea86
JendareyTechnologies <ak@jendareyconsulting.com>
2023-12-21 06:52:00 -0600

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -1,33 +1,51 @@
-variable "REGION" {
-  default = "us-east-1"
+variable "vpc_name" {
+  description = "Name of the VPC"
+  type        = string
+  default     = "jendarey_vpc"
 }
 
-variable "ZONE1" {
-  default = "us-east-1a"
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
 }
 
-variable "ZONE2" {
-  default = "us-east-1b"
+variable "availability_zones" {
+  description = "List of availability zones for the VPC"
+  type        = list(string)
+  default     = ["us-east-1a", "us-east-1b"]
 }
 
-variable "INSTANCE" {
-  type    = list(string)
-  default = ["t2.small", "t2.medium", "t2.micro"]
+variable "private_subnet_cidrs" {
+  description = "List of CIDR blocks for private subnets"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 
-variable "USER" {
-  default = "ubuntu"
+variable "public_subnet_cidrs" {
+  description = "List of CIDR blocks for public subnets"
+  type        = list(string)
+  default     = ["10.0.101.0/24", "10.0.102.0/24"]
 }
 
-variable "PUB_KEY" {
-  default = "jendareykey.pub"
+variable "enable_nat_gateway" {
+  description = "Enable NAT Gateway for private subnets"
+  type        = bool
+  default     = true
 }
 
-variable "PRIV_KEY" {
-  default = "jendareykey"
+variable "enable_vpn_gateway" {
+  description = "Enable VPN Gateway for the VPC"
+  type        = bool
+  default     = true
 }
 
-variable "COUNT" {
-  type    = list(string)
-  default = ["jendarey-dev", "jendarey-prod"]
+variable "vpc_tags" {
+  description = "Tags for the VPC resources"
+  type        = map(string)
+  default     = {
+    Terraform   = "true"
+    Environment = "prod"
+  }
 }
+
